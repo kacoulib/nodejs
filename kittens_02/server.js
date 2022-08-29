291641c71860cd3b9e9a263a906ecfffcdc179ea
kacoulib <kacoulib@gmail.com>
2022-08-29 12:36:06 +0200

    Added public directory

--- kittens_02/server.js
+++ kittens_02/server.js
@@ -1,10 +1,15 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 8000;
 const hostname = "localhost";
 
 const BASE_URL = `http://${hostname}:${port}`;
+
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(_dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Example app listening at ${BASE_URL}`);
