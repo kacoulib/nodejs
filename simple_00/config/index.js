1c26edc66061cc76578c06a0a590c08d8e54c776
kacoulib <kacoulib@gmail.com>
2022-08-31 13:34:32 +0200

    feat: post pug

--- simple_00/config/index.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const port = process.env.PORT || 3000;
-export const hostname = process.env.HOSTNAME || "localhost";
-export const sessionSecret =
-  process.env.SESSION_SECRET || "lkjdsj _sdflsdf sdfsdf98797dsfds$$pm";
